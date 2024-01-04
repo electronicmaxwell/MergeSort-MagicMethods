24f21dff3f18d422c0cb0edd292b3d7a25645e81
electronicmaxwell <95920077+electronicmaxwell@users.noreply.github.com>
2024-01-04 12:57:52 +0100

    Update load_names.py

--- load_names.py
+++ load_names.py
@@ -1,19 +1,10 @@
 from pathlib import Path
 import numpy as np
 import time
-"""
-This file provides includes a function to load in the names from the text file names.txt.
-
-Finish, the code in load_names. 
-"""
 
 def load_names():
-    """
-    Load the file names.txt and return an array or list with the names.
-    """
     # Find the text file on your computer and make it operating system insensitive.
     # path can be used to load the text file.
-    # This finds the directory where you run the python interpreter with the name __main__.
     path = Path.cwd()
     # This finds the correct path to names.txt from where you run the python file
     path = path.glob('**/names.txt').__next__()
